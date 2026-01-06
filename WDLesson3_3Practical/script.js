585fb6eaf23044c66417bf1d75e32754deab23e2
oliviak111 <oliviak111@nycstudents.net>
2026-01-06 18:05:35 +0000

    gthytgh

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function silver(){
+    document.getElementById("highlight").innerHTML = "knight"
+}
+
+function silver2(){
+    document.getElementById("highlight").innerHTML = "pajamas"
+}
+
+function silver3(){
+    document.getElementById("highlight").innerHTML = "cook off"
+}
+
+function silver4(){
+    document.getElementById("highlight").innerHTML = "dorm uniform"
+}
+
+function silver5(){
+    document.getElementById("highlight").innerHTML = "school uniform"
+}
