58718f40cff14530206927ace1fa56ea879f2ae2
oliviak111 <oliviak111@nycstudents.net>
2026-01-16 18:19:30 +0000

    WADAW

--- WDLesson3_5Practical/script.js
+++ WDLesson3_5Practical/script.js
@@ -4,11 +4,20 @@
       3) Display the results in the appropriate element
 */
 function recArea(){
-
+    let len = parseFloat(document.getElementById("l").value);
+    let wid = parseFloat(document.getElementById("w").value);
+    let op = document.getElementById("output");
+    let A = len * wid;
+    op.innerHTML = A;
 }
 
-function recPerimeter(){
 
+function recPerimeter(){
+    let len = parseFloat(document.getElementById("l").value);
+    let wid = parseFloat(document.getElementById("w").value);
+    let op = document.getElementById("output");
+    let P = 2 * (len + wid);
+    op.innerHTML = P;
 }
 
 
@@ -20,11 +29,19 @@ function recPerimeter(){
 let pi = 3.1415926;
 
 function cirArea(){
-
+      let radius = parseFloat(document.getElementById("r").value);
+      let op = document.getElementById("output");
+      let CA = pi * radius ** 2;
+      op.innerHTML = CA;
 }
 
-function cirPerimeter(){
 
+
+function cirPerimeter(){
+      let radius = parseFloat(document.getElementById("r").value);
+      let op = document.getElementById("output");
+      let CP = 2*(pi * radius);
+      op.innerHTML = CP;
 }
 
 /* Challenge Bonus: Complete the area and perimeter functions below for the triangle.  Guidelines,
--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -1,6 +1,7 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
 
 
+
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
 
 
