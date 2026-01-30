7961194b2d7a015bcd6d2ec9d669a06783a10417
oliviak111 <oliviak111@nycstudents.net>
2026-01-30 17:58:48 +0000

    asdadsasdsasd

--- WDLesson3_8Practical/script.js
+++ WDLesson3_8Practical/script.js
@@ -2,20 +2,20 @@
 //  For each challenge fix the error and explain the correction in the comment
 
 
-/* Challenge 4: Does the function name match the event handler referenced in the event listener? */
-function volume(){
+/* Challenge 4: Does the function name match the event handler referenced in the event listener? no. cylinder */
+function cylinder(){
 
-  /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs? */
-  let r = parseInt(document.getElementBy("r").value);
-  let height = parseInt(document.getElementbyId("height").value);
+  /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs? ById + height = h */
+  let r = parseInt(document.getElementById("r").value);
+  let h = parseInt(document.getElementById("h").value);
 
-  /* Challenge 6: Does the variable output correctly create a reference to the output container? */
-  let output = document.getElementById("output").value;
+  /* Challenge 6: Does the variable output correctly create a reference to the output container? no (ouput = output) + don't need .value*/
+  let output = document.getElementById("output");
 
-  /* Challenge 7: Is the following calculation for volume of a cylinder correct? */
-  let v = math.PI * math.pow(r,2) / h;
+  /* Challenge 7: Is the following calculation for volume of a cylinder correct? (*h at the end)*/
+  let v = Math.PI * Math.pow(r,2) * h;
+ // v = v.toFixed(2);
 
-  /* Challenge 8: Are there any errors in displaying the output? */
-  output.innerHTML = `Volume of the cylinder is ${v} 
-
+  /* Challenge 8: Are there any errors in displaying the output? (no ending `) */
+  output.innerHTML = `Volume of the cylinder is ${v.toFixed(4)}`;
 }
