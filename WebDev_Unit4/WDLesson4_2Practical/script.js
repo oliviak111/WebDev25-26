5f8934af59882efd1223c0c6f3f1b2448905d3e9
oliviak111 <oliviak111@nycstudents.net>
2026-02-06 18:21:36 +0000

    asdad

--- WebDev_Unit4/WDLesson4_2Practical/script.js
+++ WebDev_Unit4/WDLesson4_2Practical/script.js
@@ -13,7 +13,20 @@ function name(){
 */
 
 /* Challenge 2:  Create the function for Challenge 1 scenario */
+function area(){
+  let l = parseInt(document.getElementById("l").value);
+  let w = parseInt(document.getElementById("w").value);
+  let op = document.getElementById("output");
+  let a = l * w;
+  msg = "";
 
+  if(l <= 0 || w<=0){
+    msg = "Inappropriate measurement";
+  }else{
+    msg = `Area of ${l} and ${w} = ${a}`;
+  }
+  op.innerHTML = msg;
+}
 
 
 /* Challenge 3:  Complete the function for Pets Challenge on pets.html.  
@@ -21,5 +34,28 @@ function name(){
 2) You will need to write HTML as part of the output.  
    For example, output.innerHTML = `<input src="pic.png">` would place the image in the <div>*/
 function pet(){
+  let ani = document.getElementById("animal").value;
+  let e = document.getElementById("emotion").value;
+  let op = document.getElementById("output2");
+  let img = "";
 
+  if(ani == "Bear" && e =="Funny"){
+    img = "funnyBear.jpg";
+  }
+  if(ani == "Bear" && e =="Sad"){
+    img = "sadBear.jpg";
+  }
+  if(ani == "Cat" && e =="Funny"){
+    img = "funnyCat.jpg";
+  }
+  if(ani == "Cat" && e =="Sad"){
+    img = "sadCat.jpg";
+  }
+  if(ani == "Dog" && e =="Funny"){
+    img = "funnyDog.jpg";
+  }
+  if(ani == "Dog" && e =="Sad"){
+    img = "sadDog.jpg";
+  }
+  op.innerHTML = `<img src="${img}">`;
 }
