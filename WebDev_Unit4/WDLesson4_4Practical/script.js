ae81eaff897767acb2fe58d99feb9df667b09ae9
oliviak111 <oliviak111@nycstudents.net>
2026-02-27 17:40:24 +0000

    asd

--- WebDev_Unit4/WDLesson4_4Practical/script.js
+++ WebDev_Unit4/WDLesson4_4Practical/script.js
@@ -6,10 +6,14 @@
 */
 let correct;
 function generateProblem(){
-
+       let prob = document.getElementById("problem");
+       let roll1 = Math.floor(Math.random()*(20 - 0 +  1));
+       let roll2 = Math.floor(Math.random()*(20 - 0 +  1));
+       correct = roll1 + roll2;
+       prob.innerHTML = `${roll1} + ${roll2} = `;
 }
 
-/* Challenge 3: Complete the function checkProblem by ,
+/* Challenge 3: Complete the function checkResponse by ,
        1) Create a variable for the users response
        2) Create a variable for the <div> with id of output
        3) Create a variable msg for the output
@@ -18,8 +22,19 @@ function generateProblem(){
            b) otherwise set msg to "Incorrect. The answer is " followed by the correct answer.  Also include incorrect image
        4) Display msg in the output
 */
-function checkProblem(){
+function checkResponse(){
+       let op = document.getElementById("output");
+       let ur = parseInt(document.getElementById("response").value);
+       let message = "";
+       let filename = "";
+       if(ur == correct){
+              filename = "correct.webp";
+              message = `Correct! <br> <img src = "${filename}">`;
+       }else{
+              filename = "incorrect.avif";
+              message = `Incorrect! The answer is ${correct}. <br> <img src = "${filename}">`;
+       }
 
+       op.innerHTML = message;
+       
 }
-
-
